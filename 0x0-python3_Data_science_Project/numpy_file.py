4f88154a3bae1d99d99eee45b8e067122decb78e
Anthony <ndegwaanthony300@gmail.com>
2024-01-15 17:53:09 +0300

    added the excel and the csv datasets

---
+++ 0x0-python3_Data_science_Project/numpy_file.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+myfile = pd.read_excel("/home/anthony/data_science/Data_Science_Projects/0x0-python3_Data_science_Project/Historicaldata/Historicalinvesttemp.xlsx")
+myfile1 = pd.read_csv("/home/anthony/data_science/Data_Science_Projects/0x0-python3_Data_science_Project/African/African-Nations-results.csv")
+myfile1
